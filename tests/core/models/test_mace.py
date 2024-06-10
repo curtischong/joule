c8c0ffacc4d4b31ac7e71f8ef363f4f3059f535f
Curtis Chong <curtischong5@gmail.com>
2024-06-10 18:53:27 -0400

    added test mace file

---
+++ tests/core/models/test_mace.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+class TestMace:
+    def test_not_mixing_batch_dim(self):
+        pass
+
+    # I think it's fine if we just test with on an untrained model
+    def test_equivariance(self):
+        pass
