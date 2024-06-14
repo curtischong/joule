54bd0d895029b56e4941a7450790877e87a6b1b2
Curtis Chong <curtischong5@gmail.com>
2024-06-14 08:51:22 -0400

    Parallelize db load (#19)
    * code to parallelize parsing the training files
    
    * use threads instead
    
    * helping parallelize

--- scripts/dataset_prep/create_real_mace_dataset_lmdb2.py
+++ scripts/dataset_prep/create_real_mace_dataset_lmdb2.py
@@ -15,34 +15,49 @@ from pymatgen.entries.computed_entries import ComputedStructureEntry
 from pymatgen.io.ase import AseAtomsAdaptor
 from ase.calculators.singlepoint import SinglePointCalculator
 import h5py
+from multiprocessing import Process
+import concurrent.futures
 
 from dataset_prep_common import get_range, parse_config
 
 IN_TRAIN_DIR = "datasets/real_mace/train"
 IN_VAL_DIR = "datasets/real_mace/val"
-TRAIN_DIR = "datasets/lmdb/real_mace/train"
-VAL_DIR = "datasets/lmdb/real_mace/val"
+OUT_TRAIN_DIR = "datasets/lmdb/real_mace/train"
+OUT_VAL_DIR = "datasets/lmdb/real_mace/val"
 # MAX_ATOMIC_NUMBER = 36
 MAX_ATOMIC_NUMBER = 54
+MAX_JOBS = 8
 
 
 def main():
     config = parse_config()
 
-    os.makedirs(TRAIN_DIR, exist_ok=True)
-    os.makedirs(VAL_DIR, exist_ok=True)
+    os.makedirs(OUT_TRAIN_DIR, exist_ok=True)
+    os.makedirs(OUT_VAL_DIR, exist_ok=True)
 
-    num_train_files = 64
-    for i in range(num_train_files):
-        entries = get_entries(IN_TRAIN_DIR, f"train_{i}")
-        db_name = f"{TRAIN_DIR}/{i}"
-        create_lmdb(config, db_name, entries)
 
-    num_val_files = 64
-    for i in range(num_val_files):
-        entries = get_entries(IN_VAL_DIR, f"val_{i}")
-        db_name = f"{VAL_DIR}/{i}"
+    parse_datasets(config, IN_TRAIN_DIR, OUT_TRAIN_DIR, "train", num_files=64)
+    # parse_datasets(config, IN_VAL_DIR, OUT_VAL_DIR, "val", num_files=64)
+
+
+def parse_datasets(config, in_dir, out_dir, in_dir_prefix, num_files):
+    def process_file(i):
+        entries = get_entries(in_dir, f"{in_dir_prefix}_{i}")
+        db_name = f"{out_dir}/{i}"
         create_lmdb(config, db_name, entries)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_JOBS) as executor:
+        futures = []
+        for i in range(num_files):
+            futures.append(executor.submit(process_file, i))
+        
+        # Wait for all futures to complete
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                future.result()  # to raise any exceptions occurred
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
 
 def create_lmdb(config, dataset_path, atoms: list[pymatgen.io.ase.MSONAtoms]):
     db = lmdb.open(
@@ -66,6 +81,8 @@ def create_lmdb(config, dataset_path, atoms: list[pymatgen.io.ase.MSONAtoms]):
 
     tags = atoms[0].get_tags()
     data_objects = a2g.convert_all(atoms, disable_tqdm=True)
+
+    print(f"reading {dataset_path}")
 
 
     for fid, data in tqdm(enumerate(data_objects), total=len(data_objects)):
@@ -107,6 +124,7 @@ def get_entries(in_dir, file_name):
 
     with h5py.File(f"{in_dir}/{file_name}.h5", 'r') as hdf5_file:
         num_configs = len(hdf5_file["config_batch_0"])
+        # num_configs = 1000
         for i in tqdm(range(num_configs)):
             config_group = hdf5_file[f'config_batch_0/config_{i}']
             atomic_numbers = config_group['atomic_numbers'][:]
