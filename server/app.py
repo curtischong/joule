d698ac125a7cca59b93567c8c18c57e957de02b7
Curtis Chong <curtischong5@gmail.com>
2024-06-17 23:30:44 -0400

    error handling for atomic numbers that the model isn't trained on

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from ase import Atoms
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from fairchem.core.common.relaxation.ase_utils import OCPCalculator
 from flask_cors import CORS
+import json
+from pymatgen.core.periodic_table import Element
 
 app = Flask(__name__)
 CORS(app)  # Allow all origins
@@ -9,11 +11,23 @@ CORS(app)  # Allow all origins
 checkpoint_path = "models/checkpoint_small.pt"
 calculator = OCPCalculator(checkpoint_path=checkpoint_path)
 
+most_common_elements_only_one_per_sample = [8, 3, 15, 12, 16, 1, 25, 7, 26, 14, 9, 6, 29, 27, 11, 23, 19, 20, 13, 17] 
+
+
 @app.route("/predict", methods=["POST"])
 def calculate():
     data = request.get_json()
     crystal = data["crystal"]
-    atoms = Atoms(symbols=crystal["atomicNumbers"], scaled_positions=crystal["fracCoords"], cell=crystal["latticeMatrix"], pbc=[True, True, True])
+    atomic_numbers = crystal["atomicNumbers"]
+
+    # validate that we can handle this atomic number
+    for atomic_number in atomic_numbers:
+        if atomic_number not in most_common_elements_only_one_per_sample:
+            response = jsonify(errorMsg=f"the model is not trained to handle element {Element.from_Z(atomic_number)}")
+            response.status_code = 400
+            return response
+
+    atoms = Atoms(symbols=atomic_numbers, scaled_positions=crystal["fracCoords"], cell=crystal["latticeMatrix"], pbc=[True, True, True])
     calculator.calculate(atoms, properties=["energy", "forces"], system_changes=[]) # I don't even think that system_changes is used
     energy = calculator.results["energy"]
     forces = calculator.results["forces"].tolist()
