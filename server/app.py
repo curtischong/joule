4d211b85469e05a4ca7484b2d567790cc500abeb
Curtis Chong <curtischong5@gmail.com>
2024-06-17 22:16:27 -0400

    cleanup

--- server/app.py
+++ server/app.py
@@ -1,28 +1,13 @@
 from ase import Atoms
 from flask import Flask, request
-# import argparse
-# from fairchem.core.common.flags import flags
 from fairchem.core.common.relaxation.ase_utils import OCPCalculator
-# from fairchem.core.common.utils import (
-#     build_config,
-# )
-# from fairchem.core._cli import Runner
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Allow all origins
 
-# parser: argparse.ArgumentParser = flags.get_parser()
-# args, override_args = parser.parse_known_args()
-# config = build_config(args, override_args)
-# Runner()(config)
-config_yml = "configs/s2ef/all/joule/upgraded_escn.yml"
 checkpoint_path = "models/checkpoint_small.pt"
-calculator = OCPCalculator(
-    # config_yml=config_yml,
-    checkpoint_path=checkpoint_path,
-    # trainer="forces"
-)
+calculator = OCPCalculator(checkpoint_path=checkpoint_path)
 
 @app.route("/predict", methods=["POST"])
 def calculate():
